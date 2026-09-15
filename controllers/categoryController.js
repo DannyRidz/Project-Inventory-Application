fa9026f65f17541b59827f7b3e27729952c2e2c2
DannyRidz <danny.ridz93@gmail.com>
2026-09-15 20:40:40 +0800

    feat: enhance category management with detailed views and database queries

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,9 +1,31 @@
-exports.categoryList = (req, res) => {
-  res.send("List of categories");
+const db = require("../db/queries");
+
+exports.categoryList = async (req, res) => {
+  const categories = await db.getAllCategories();
+
+  res.render("categories/index", {
+    title: "Categories",
+    categories,
+  });
 };
 
-exports.categoryDetail = (req, res) => {
-  res.send(`Category details for ID: ${req.params.id}`);
+exports.categoryDetail = async (req, res) => {
+  const categoryId = req.params.id;
+
+  const [category, games] = await Promise.all([
+    db.getCategoryById(categoryId),
+    db.getGamesByCategoryId(categoryId),
+  ]);
+
+  if (!category) {
+    return res.status(404).send("Category not found");
+  }
+
+  res.render("categories/detail", {
+    title: category.name,
+    category,
+    games,
+  });
 };
 
 exports.categoryCreateGet = (req, res) => {
--- db/queries.js
+++ db/queries.js
@@ -8,6 +8,29 @@ async function getAllCategories() {
   return rows;
 }
 
+async function getCategoryById(id) {
+  const { rows } = await pool.query(
+    "SELECT id, name, description FROM categories WHERE id = $1",
+    [id],
+  );
+
+  return rows[0];
+}
+
+async function getGamesByCategoryId(categoryId) {
+  const { rows } = await pool.query(
+    `SELECT id, title, price, stock_quantity
+     FROM games
+     WHERE category_id = $1
+     ORDER BY title`,
+    [categoryId],
+  );
+
+  return rows;
+}
+
 module.exports = {
   getAllCategories,
+  getCategoryById,
+  getGamesByCategoryId,
 };
